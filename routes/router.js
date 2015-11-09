e50a6e1fcf7875f0111010544d356741bdf55baf
Chris Randle <crandle@ucsd.edu>
2015-11-09 14:13:58 -0800

    Post Tutorial advanced novice

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,9 @@
 //This is where the websites navigation happens
-function route(handle, pathname, response) {
+function route(handle, pathname, response, postData) {
     console.log("Prepare to route a request " + pathname);
     //Check to see if the requested url path exists
     if (typeof handle[pathname] === 'function') {
-        handle[pathname](response);
+        handle[pathname](response, postData);
     } else {
         console.log("No request handler found for " + pathname);
         response.writeHead(404, {"Content-Type": "text/plain"});
--- server.js
+++ server.js
@@ -3,10 +3,27 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
+        //Place Holder for incoming data from POST operations
+        var postData = "";
+        //Parse incoming URL requests for processing by the router
         var pathname = url.parse(request.url).pathname;
+        //LOG Actions
         console.log("Request for " + pathname + " received.");
 
-        route(handle, pathname, response);
+        //Assume encoding of received data will be utf8
+        request.setEncoding("utf8");
+
+        //Listener for recording POST data
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+            //Don't keep this log line for production COULD GET REALLY BIG
+            console.log("Received POST data chunk '" + postDataChunk + "'.");
+        });
+
+        //After all POST data is gathered route the request
+        request.addListener("end", function() {
+            route(handle, pathname, response, postData);
+        });
     }
     http.createServer(onRequest).listen(8888);
     console.log("Server has started.");
