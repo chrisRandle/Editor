03c0a7c897244ec540b99ff94d401fb77bd178ad
Chris Randle <crandle@ucsd.edu>
2015-11-09 11:40:55 -0800

    Add base request handlers

---
+++ routes/requestHandlers.js
@@ -0,0 +1,13 @@
+/**
+ * Created by chrisrandle on 11/9/15.
+ */
+function start() {
+    console.log("Request handler 'start' was called.");
+}
+
+function upload() {
+    console.log("Request handler 'upload' was called.");
+}
+
+exports.start = start;
+10 exports.upload = upload;
